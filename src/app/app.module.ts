39d7302d67082286b070574e348cf938758e2ac0
Rajesh <t.rajesh@lotuswireless.com>
2020-08-09 08:32:01 +0530

    credit card forms added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CardFormComponent } from './card-form/card-form.component';
+import { InputComponent } from './input/input.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    InputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
